9d5e974a74959cd71d0a317e38ca1233faf6a149
jmcquown <jmcq521@gmail.com>
2017-01-24 18:48:46 -0500

    Implemented word count for commit messages

--- commit_word_count.go
+++ commit_word_count.go
@@ -25,6 +25,7 @@ func main() {
     bytePassword, _ := terminal.ReadPassword(int(syscall.Stdin))
     password := string(bytePassword)
 
+    //Authenticate the user
     tp := github.BasicAuthTransport{
         Username: strings.TrimSpace(username),
         Password: strings.TrimSpace(password),
@@ -34,16 +35,38 @@ func main() {
 
     username = strings.TrimSpace(username)
 
-    // fmt.Printf("\n%v\n", commitString)
-    fmt.Printf("\n%v\n", getCommitMessages(username, client))
+    //messages hold one long concatenated string of all the user's commit messages
+    messages := getCommitMessages(username, client)
+
+
+    fmt.Print("\n")
+    //Iterate through the map and print each key/value pair
+    wordCountMap := wordCount(messages)
+    for key, value := range wordCountMap {
+        fmt.Printf("%v:\t%v\n", key, value)
+    }
+}
+
+//Gets word count and return a map with Key - string Value - int
+func wordCount(words string) map[string]int {
+    //Split each word in words by " " and return a list of words
+    wordList := strings.Fields(words)
+    counts := make(map[string]int)
+
+    for i := range wordList {
+        counts[wordList[i]]++
+    }
+
+    return counts
 }
 
 func getCommitMessages(username string, client *github.Client) string {
     commitString := ""
     reposList, _, _ := client.Repositories.List("", nil)
 
-
+    //Iterate through the list of repositories
     for _, repo := range reposList {
+        //Get the name of each repository from the list (actually a list of structs)
         repoName := *repo.Name
 
         //List the commits on each repository 
